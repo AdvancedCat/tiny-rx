1376502ce790624d8f6d497df122a64da985c947
advancedcat <wshx1938@gmail.com>
2022-03-25 22:23:55 +0800

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,115 @@
-// remove test if you change code here
-export function foo() {
-    return 'Hello World'
+const NOOP = function () {}
+export default class Observable {
+    static operators
+    _subscribe
+    constructor(subscribe) {
+        this._subscribe = subscribe
+    }
+
+    pipe(...operators) {
+        return operators.reduce((prev, fn) => {
+            fn(prev)
+        }, this)
+    }
+
+    subscribe(observer) {
+        const defaultObserver = {
+            next: NOOP,
+            error: NOOP,
+            complete: NOOP,
+        }
+
+        if (typeof observer === 'function') {
+            return this._subscribe({
+                ...defaultObserver,
+                next: observer,
+            })
+        } else {
+            return this._subscribe({
+                ...defaultObserver,
+                ...observer,
+            })
+        }
+    }
+}
+
+const emptyObservable = new Observable(NOOP)
+
+// create operators
+export function of(...args) {
+    return new Observable((observer) => {
+        try {
+            args.forEach((arg) => {
+                observer.next(arg)
+            })
+        } catch (err) {
+            observer.error(err)
+        } finally {
+            observer.complete()
+        }
+        return {
+            unsubscribe: NOOP,
+        }
+    })
+}
+
+export function fromEvent(dom, eventName) {
+    if (!dom || !eventName) return emptyObservable
+
+    return new Observable(function (observer) {
+        const handler = (evt) => observer.next(evt)
+        dom.addEventListener(eventName, handler)
+        return {
+            unsubscribe: () => {
+                dom.removeEventListener(eventName, handler)
+            },
+        }
+    })
+}
+
+// filter operators
+export function map(fn) {
+    return (observable) =>
+        new Observable((observer) => {
+            observable.subscribe({
+                next: (val) => observer.next(fn(val)),
+                error: (err) => observer.error(err),
+                complete: () => observer.complete(),
+            })
+        })
+}
+
+export function filter(fn) {
+    return (observable) =>
+        new Observable((observer) => {
+            observable.subscribe({
+                next: (val) => (fn(val) ? observer.next(val) : NOOP()),
+                error: (err) => observer.error(err),
+                complete: () => observer.complete(),
+            })
+        })
+}
+
+export function take(count) {
+    return (observable) =>
+        new Observable((observer) => {
+            let times = 0
+            const subscription = observable.subscribe({
+                next: function (val) {
+                    times++
+                    if (times <= count) {
+                        observer.next(val)
+                    } else {
+                        observer.complete()
+                        subscription.unsubscribe()
+                    }
+                },
+                error: (err) => observer.error(err),
+                complete: () => observer.complete(),
+            })
+        })
+}
+
+Observable.operators = {
+    of,
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,32 @@
-import { foo } from '../src'
+import Observable, { of } from '../src'
 
-test('Hello World', () => {
-    expect(foo()).toBe('Hello World')
+describe('Observable', () => {
+    test('construct', () => {
+        const fn = jest.fn()
+        const ob = new Observable(fn)
+
+        expect('_subscribe' in ob).toBe(true)
+        expect(ob._subscribe === fn).toBe(true)
+    })
+
+    test('subscribe an observer', () => {
+        let flag = false
+        const cb = function (observer) {
+            observer.next(1)
+        }
+        const ob = new Observable(cb)
+        ob.subscribe({
+            next: () => {
+                flag = true
+            },
+        })
+
+        expect(flag).toBe(true)
+    })
 })
+
+// describe('Operators', ()=>{
+//     test('of', ()=>{
+
+//     })
+// })
